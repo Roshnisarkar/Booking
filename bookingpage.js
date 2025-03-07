b0f9005c1dc5712409c0afdce51af1b2087435a2
Roshnisarkar <roshnisarkardev20@gmail.com>
2025-03-07 15:04:03 +0530

    first commit

---
+++ bookingpage.js
@@ -0,0 +1,118 @@
+let bt=()=>{
+  let but=  document.querySelector(".btn")
+  but.style.backgroundColor="green"
+
+  if (but.style.backgroundColor === 'green') {
+    but.style.backgroundColor = 'none'; // Revert to default color
+  } else {
+    but.style.backgroundColor = 'green'; // Change to green
+  }
+
+}
+
+let bt1=()=>{
+     let a=  document.querySelector(".btn1")
+    a.style.backgroundColor="green"
+  }
+
+  let bt2=()=>{
+    let b=  document.querySelector(".btn2")
+    b.style.backgroundColor="green"
+  }
+
+  let bt3=()=>{
+    let c=  document.querySelector(".btn3")
+    c.style.backgroundColor="green"
+  }
+  let bt4=()=>{
+    let d=  document.querySelector(".btn4")
+    d.style.backgroundColor="green"
+  }
+  let bt5=()=>{
+    let e=  document.querySelector(".btn5")
+    e.style.backgroundColor="green"
+  }
+  let bt6=()=>{
+    let f=  document.querySelector(".btn6")
+   f.style.backgroundColor="green"
+  }
+  let bt7=()=>{
+    let g=  document.querySelector(".btn7")
+    g.style.backgroundColor="green"
+  }
+  let bt8=()=>{
+    let h=  document.querySelector(".btn8")
+  h.style.backgroundColor="green"
+  }
+  let bt9=()=>{
+    let i=  document.querySelector(".btn9")
+    i.style.backgroundColor="green"
+  }
+  let bt10=()=>{
+    let j=  document.querySelector(".btn10")
+    j.style.backgroundColor="green"
+  }
+  let bt11=()=>{
+    let k=  document.querySelector(".btn11")
+   k.style.backgroundColor="green"
+  }
+  let bt12=()=>{
+    let l=  document.querySelector(".btn12")
+   l.style.backgroundColor="green"
+  }
+
+  let bt13=()=>{
+    let m=  document.querySelector(".btn13")
+    m.style.backgroundColor="green"
+  }
+  
+  let bt14=()=>{
+       let n=  document.querySelector(".btn14")
+      n.style.backgroundColor="green"
+    }
+  
+    let bt15=()=>{
+      let o=  document.querySelector(".btn15")
+      o.style.backgroundColor="green"
+    }
+  
+    let bt16=()=>{
+      let p=  document.querySelector(".btn16")
+      p.style.backgroundColor="green"
+    }
+    let bt17=()=>{
+      let q=  document.querySelector(".btn17")
+      q.style.backgroundColor="green"
+    }
+    let bt18=()=>{
+      let r=  document.querySelector(".btn18")
+      r.style.backgroundColor="green"
+    }
+    let bt19=()=>{
+      let s=  document.querySelector(".btn19")
+     s.style.backgroundColor="green"
+    }
+    let bt20=()=>{
+      let t=  document.querySelector(".btn20")
+      t.style.backgroundColor="green"
+    }
+    let bt21=()=>{
+      let u=  document.querySelector(".btn21")
+    u.style.backgroundColor="green"
+    }
+    let bt22=()=>{
+      let v=  document.querySelector(".btn22")
+      v.style.backgroundColor="green"
+    }
+    let bt23=()=>{
+      let w=  document.querySelector(".btn23")
+      w.style.backgroundColor="green"
+    }
+    let bt24=()=>{
+      let x=  document.querySelector(".btn24")
+     x.style.backgroundColor="green"
+    }
+    let bt25=()=>{
+      let z=  document.querySelector(".btn25")
+     z.style.backgroundColor="green"
+    }
---
+++ index.js
@@ -0,0 +1,57 @@
+
+let signup =()=>{
+    let nam=document.querySelector("#firstname").value
+    let last=document.querySelector("#lastname").value
+    let email=document.querySelector("#email-inp").value
+    let pass=document.querySelector("#pass-inp").value
+    let er=document.querySelector("#er")
+    if(nam==""){
+        er.innerHTML="fill the input field"
+        return false
+    }
+else if( last=="" ){
+    er.innerHTML="fill the input field"
+    return false
+}
+
+else if( email=="" ){
+    er.innerHTML="fill the input field"
+    return false
+}
+else if( pass=="" ){
+    er.innerHTML="fill the input field"
+    return false
+}
+
+
+localStorage.setItem("name",nam)
+localStorage.setItem("lastname",last)
+localStorage.setItem("email",email)
+localStorage.setItem("password",pass)
+
+location.href="homepagge.html"
+
+return false
+
+}
+
+let login =()=>{
+    // let n =document.querySelector("#name").value
+    // let l=document.querySelector("#lname").value
+    let e=document.querySelector("#semail").value
+    let p=document.querySelector("#spass").value
+    let er=document.querySelector("#err")
+
+ // let lfname=localStorage.getItem("name")
+// let lsname=localStorage.getItem("lastname")
+let lsemail=localStorage.getItem("email")
+let lspass=localStorage.getItem("password")
+
+if( e==lsemail && p==lspass)
+    location.href="homepagge.html"
+
+return false
+
+}
+
+
